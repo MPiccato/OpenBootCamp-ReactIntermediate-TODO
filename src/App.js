47191404ab8c1783b84778dff14ba4dd8ceb2bdf
mpiccato <mpiccato@me.com>
2023-01-22 18:27:30 -0300

    terminado ejemplo4 sobre el tratamiento al props.children

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 //import Ejemplo1 from './hooks/Ejemplo1';
 //import Ejemplo2 from './hooks/Ejemplo2';
 import MiComponenteConContexto from './hooks/Ejemplo3';
+import Ejemplo4 from './hooks/Ejemplo4';
 
 function App() {
   return (
@@ -24,7 +25,15 @@ function App() {
         {/* Ejemplo 2 de uso de useState, useRef, useEffect */}
         {/* <Ejemplo2/> */}
         {/* Ejemplo3 con useContext */}
-        <MiComponenteConContexto/>
+        {/* <MiComponenteConContexto/> */}
+        {/* Ejemplo4 uso de children */}
+        <Ejemplo4 nombre="Martin">
+          
+          <h3>
+            Contenido de props.children
+          </h3>
+        </Ejemplo4>
+
 
       </header>
     </div>
