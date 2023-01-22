bac472e5833d59ca2a244c45715680a3c5e04d4a
mpiccato <mpiccato@me.com>
2023-01-22 19:01:51 -0300

    agregado node-sass

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 //import TaskListComponent from './components/container/task_list';
 //import Ejemplo1 from './hooks/Ejemplo1';
 //import Ejemplo2 from './hooks/Ejemplo2';
-import MiComponenteConContexto from './hooks/Ejemplo3';
+//import MiComponenteConContexto from './hooks/Ejemplo3';
 import Ejemplo4 from './hooks/Ejemplo4';
 
 function App() {
